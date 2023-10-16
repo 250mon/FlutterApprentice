23ada9c19f3def24990bf078e79c92780edcc3d7
Jye Young Song <lambki@naver.com>
2023-10-17 01:33:58 +0900

    Ch2 Finished Again

--- recipes/lib/main.dart
+++ recipes/lib/main.dart
@@ -21,8 +21,7 @@ class RecipeApp extends StatelessWidget {
         ),
         // useMaterial3: true,
       ),
-      // home: const MyHomePage(title: 'Recipe Calculator'),
-      home: const Home()
+      home: const MyHomePage(title: 'Recipe Calculator'),
     );
   }
 }
@@ -38,8 +37,23 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values.
+      _counter++;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    // This method is rerun every time setState is called
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
@@ -54,8 +68,7 @@ class _MyHomePageState extends State<MyHomePage> {
                       context,
                       MaterialPageRoute(
                           builder: (context) {
-                          // TODO: Replace return with return RecipeDetail()
-                            return Text('Detail page');
+                            return RecipeDetail(recipe: Recipe.samples[index]);
                           },
                       ),
                     );
