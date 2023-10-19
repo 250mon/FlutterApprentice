8b324c1fd8d6b7af97717a80a5828f26deff1c5e
Jye Young Song <lambki@naver.com>
2023-10-20 07:36:39 +0900

    Card2 done

--- fooderlich/lib/card2.dart
+++ fooderlich/lib/card2.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:fooderlich/author_card.dart';
+import 'fooderlich_theme.dart';
 
 class Card2 extends StatelessWidget {
   const Card2({super.key});
@@ -22,7 +24,36 @@ class Card2 extends StatelessWidget {
         ),
         child: Column(
           children: [
-
+            const AuthorCard(
+              authorName: 'Jye Song',
+              title: 'Smootie Connoisseur',
+              imageProvider: AssetImage('assets/cinnamon.png'),
+            ),
+            Expanded(
+                child: Stack(
+                  children: [
+                    Positioned(
+                      bottom: 16,
+                      right: 16,
+                      child: Text(
+                        'Recipe',
+                        style: FooderlichTheme.lightTextTheme.displayLarge,
+                      ),
+                    ),
+                    Positioned(
+                      bottom: 70,
+                      left: 16,
+                      child: RotatedBox(
+                        quarterTurns: 3,
+                        child: Text(
+                          'Smoothies',
+                          style: FooderlichTheme.lightTextTheme.displayLarge,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+            ),
           ],
         ),
       ),
