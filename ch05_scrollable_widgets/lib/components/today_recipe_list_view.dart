6575996e15b30951e663ebaed0857594aee202bb
Jye Young Song <lambki@naver.com>
2023-10-31 08:31:22 +0900

    ch5 starting

--- ch05_scrollable_widgets/lib/components/today_recipe_list_view.dart
+++ ch05_scrollable_widgets/lib/components/today_recipe_list_view.dart
@@ -13,6 +13,46 @@ class TodayRecipeListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(
+        left: 16,
+        right: 16,
+        top: 16,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Recipes of the Day',
+            style: Theme.of(context).textTheme.displayLarge
+          ),
+          const SizedBox(height: 16),
+          Container(
+            height: 400,
+            color: Colors.transparent,
+            child: ListView.separated(
+                itemBuilder: (context, index) {
+                  final recipe = recipes[index];
+                  return buildCard(recipe);
+                },
+                separatorBuilder: (context, index) {
+                  return const SizedBox(width: 16);
+                },
+                itemCount: recipes.length,
+                scrollDirection: Axis.horizontal,
+            )
+          ),
+        ],
+      ),
+    );
+  }
 
+  Widget buildCard(ExploreRecipe recipe) {
+    return switch (recipe.cardType) {
+      RecipeCardType.card1 => Card1(recipe: recipe),
+      RecipeCardType.card2 => Card2(recipe: recipe),
+      RecipeCardType.card3 => Card3(recipe: recipe),
+      _ => throw Exception()
+    };
   }
 }
